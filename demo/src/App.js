56de4bccc5990426c79aac86a0fba95826bbbed1
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2019-08-25 13:01:14 +0200

    Add client-side routing to demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { HashRouter, Route, Link, Redirect, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
-function App() {
-  return <div>snackstack Demo</div>;
-}
+const App = () => {
+  return (
+    <HashRouter basename="/" hashType="noslash">
+      This is currently just for testing client-side routing with gh-pages
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
+    </HashRouter>
+  );
+};
 
 export default App;
---
+++ demo/src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <span>Home</span>;
+};
+
+export default Home;
