c4bd449d4350d0b33f7ca3cefd10d9a3d656821d
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2022-07-08 13:21:25 +0200

    Update packages and remove HOC

--- src/hocs/index.ts
+++
@@ -1,1 +0,0 @@
-export { withSnacks } from './withSnacks';
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 export { useSnacks } from './hooks';
-export { withSnacks } from './hocs';
 export { SnackProvider } from './SnackProvider';
 
 export { SnackProviderOptions } from './types/SnackProviderOptions';
 export { SnackRendererProps } from './types/SnackRendererProps';
-
-// cache test
