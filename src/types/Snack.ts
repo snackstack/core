30b3fe06f4b993a80b536b836eb36d9902cf3c7a
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2022-07-09 12:50:01 +0200

    Change message type

--- src/types/Snack.ts
+++ src/types/Snack.ts
@@ -4,7 +4,7 @@ type SnackStatus = 'open' | 'closing' | 'inactive';
 
 export type Snack = {
   readonly id: string | number;
-  message: React.ReactNode;
+  message: React.ReactNode | ((snack: Snack) => React.ReactNode);
   readonly variant: SnackVariant;
   persist: boolean;
   status: SnackStatus;
